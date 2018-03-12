b5da19f0c1528ec8b355b0bf12c3267bb94aca3c
Robert <robert_dipp@hotmail.com>
2018-03-11 20:45:41 -0400

    Set up project.

--- Book-Klub/ViewController.swift
+++ Book-Klub/ViewController.swift
@@ -7,17 +7,15 @@
 //
 
 import UIKit
+import GoogleBooksApiClient
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        print("Hello World!")
+        let session = URLSession.shared
+        let client = GoogleBooksApiClient(session: session)
     }
 
 
